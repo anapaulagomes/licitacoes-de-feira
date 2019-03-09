1aa89dd1371f55963626f305e88f2fdffe213433
Ana Paula Gomes <apgomes88@gmail.com>
2019-03-09 13:25:52 +0100

    Make parser more flexible

--- data_collection/contracts/parse_contract.py
+++ data_collection/contracts/parse_contract.py
@@ -1,5 +1,4 @@
 import os
-import pandas as pd
 import re
 import requests
 import requests_cache
@@ -9,16 +8,27 @@ from io import StringIO
 ONE_DAY_IN_SECONDS = 86400
 CNPJ_PATTERN = re.compile(r'\d{2}\.\d{3}\.\d{3}[\/|.]\d{4}-\d{2}')
 CPF_PATTERN = re.compile(r'\d{3}\.\d{3}\.\d{3}-\d{2}')
-CNPJ_OR_CPF_PATTERN = re.compile(r'\d{2}\.\d{3}\.\d{3}[\/|.]\d{4}-\d{2}|\d{3}\.\d{3}\.\d{3}-\d{2}')
+CNPJ_OR_CPF_PATTERN = re.compile(r'\d{2}\S+\d{3}\S+\d{3}\S+\d{4}.*?\d{2}|\d{3}\S+\d{3}\S+\d{3}.*?\d{2}')
 CONTRACT_HEADER_PATTERN = re.compile(r'do outro lado,(.*) |cláusulas e condições seguintes')
-LEGAL_REPRESENTANT_PATTERN = re.compile(r'representante legal,?(.+?),')
+LEGAL_REPRESENTANT_PATTERN = re.compile(r'representante .+? (.+?),')
 
 requests_cache.install_cache(cache_name='contracts/ocrspace_cache', backend='sqlite', expire_after=ONE_DAY_IN_SECONDS)
 
 
-def clean_string(string):
+def clean_document(string):
     string = re.sub(r'[^\w]', ' ', string)
+    string = string.replace(' ', '')
     return string.lstrip().rstrip()
+
+def clean_string(string):
+    words = string.split(' ')
+    # removing symbols in the middle of the name
+    for word in words:
+        string = re.sub(r'[^\w]', ' ', string)
+    return ' '.join([
+        re.sub(r'[^\w]', ' ', word).replace(' ', '')
+        for word in words
+    ])
 
 
 def parse_name(header):
@@ -33,7 +43,6 @@ def parse_legal_representant(header):
         if salutation in header:
             header = header.replace(salutation, '')
     legal_representant = re.findall(LEGAL_REPRESENTANT_PATTERN, header)
-
     if legal_representant != []:
         return clean_string(legal_representant[0])
     return ''
@@ -42,9 +51,11 @@ def parse_legal_representant(header):
 def parse_documents(header):
     documents = re.findall(CNPJ_OR_CPF_PATTERN, header)
     if documents:
-        cleaned_document = clean_string(documents[0])
-        return cleaned_document.replace(' ', '')
-    return None
+        return [
+            clean_document(document)
+            for document in documents
+        ]
+    return []
 
 
 def find_documents_and_names(sample):
@@ -52,19 +63,20 @@ def find_documents_and_names(sample):
     if header:
         header = header[0]
         name = parse_name(header)
+
         index_name = header.find(name)
-        header = header[index_name:]
+        header_without_name = header[index_name:]
         
-        legal_representant = parse_legal_representant(header)
-
-        document_header = re.findall(f'{name}(.*){legal_representant}', header)
+        legal_representant = parse_legal_representant(header_without_name)
+        documents = parse_documents(header)
         document = None
-        if document_header:
-            document_header = document_header[0]
-            document = parse_documents(document_header)
-            header = header.replace(document_header, '')
-
-        legal_representant_document = parse_documents(header)
+        legal_representant_document = None
+        if documents:
+            document = documents[0]
+            try:
+                legal_representant_document = documents[1]
+            except:
+                pass
 
         return {
             'name': name,
@@ -102,11 +114,29 @@ def parse_contract(response):
     return parsed_contract
 
 
-if __name__ == '__main__':
-    urls = pd.read_csv('data/contracts-url-feira-de-santana-2016-2017.csv')
+def parse_contracts(urls):
+    for url in urls:
+        if '.pdf' in url:
+            response = retrieve_content_from_pdf(url)
+            parsed_contract = parse_contract(response)
+            # print(parsed_contract)
+            filename = url[url.rfind('/')+1:]
+            print(filename, find_documents_and_names(parsed_contract))
 
-    for _, document in urls.iterrows():
-        response = retrieve_content_from_pdf(document['url'])
-        parsed_contract = parse_contract(response)
-        # print(parse_contract)
-        print(find_documents_and_names(parsed_contract))
+
+if __name__ == '__main__':
+    urls = open('data/contracts/contracts-url-feira-de-santana-2016-2017.csv').readlines()
+    # urls = [
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134504000000.pdf',
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134448000000.pdf',
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134430000000.pdf',
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134411000000.pdf',
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134351000000.pdf',
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134333000000.pdf',
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134248000000.pdf',
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134224000000.pdf',
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134207000000.pdf',
+    #     'http://www.transparencia.feiradesantana.ba.gov.br/contratos/06092017134147000000.pdf'
+
+    # ]
+    parse_contracts(urls)
--- data_collection/tests/test_parse_contract.py
+++ data_collection/tests/test_parse_contract.py
@@ -1,5 +1,5 @@
 import pytest
-from data_collection.contracts.parse_contract import find_documents_and_names
+from data_collection.contracts.parse_contract import find_documents_and_names, parse_documents
 
 
 def test_find_documents_and_names_contract1():
@@ -60,3 +60,24 @@ def test_find_documents_and_names_contract5():
         'legal_representant_document': '13099647500'
     }
     assert find_documents_and_names(sample.read()) == expected_data
+
+
+def test_find_documents_and_names_contract_with_symbol():
+    sample = "PREFEITURA MUNICIPAL OE FEIRA DE SANTANA Secretaria Municipal de Administração Departamento de Licitação e Contratos CONTRATO RO 106 — 2017— WC Contrato que entre si de um lado, o MUNICÍPIO DE FEIRA DE SANTANA, jurídka de direito púdto interno, inwita no CNPJ sob o no 14.043.574/0001-51, com sede na Av. Senhor dos Pasos, no 980, Cmtro - Era de pelo Sr. José Ronaldo de Grva\'ho, autorizado art. 86, XIV, da sua Lei Orgânica, dorava denominado CONTRATANTE e, do outro lado, M E B SERVICO E LOCACOES LTDA - ME , estabelecida na Rua Barão dc Cotegipe, no 1 14, bairro, Centro, cidade Feira de Santana, inscrtã no CND sab no 05.824.468.0001-14 atrav& do seu representante le-ga!, Sr.(a) DORANE[DE DOS REIS SOARFS, innito no CPF sob o no 480.017.085-00, denomgnada CONTRATADA, observada a Licita*o no 037-2017, Pregão Prxendal na 032•2017, que regerá Lei Federal no. 10.520, de 17 de julho de 2002; Lei Municipal no. 2.593/05 de 07 de julho de 2005; Lei Estadual no. 9.433/05 de OI de março de 2005; Lei Federal no. 123, de 14 de dezembm de 2006; Decreto Municipal no. 7.583, de 05 de setembro de 2008; Lei F&al no. 8.666, de 21 de junho de 1993, com alteraÑe posteriores e dernals normas aptlcáveis à sp&ie, as c%usulas e CLÁUSULA PRIMEIRA - OBJETO CONTRATAÇÃO DE EMPRESA ESPECIALIZADA NA PRESTAÇÃO DE SEMÇO DE IMPRESSÕES E CÓPIAS REPROGRÁFICAS, PARA ATENDER AS NECESSIDADES DA SECRETARIA MUNICIPAL DE EDUCAÇÃO E DAS SCOLAS MUNICIPAIS, PELO PERÍODO DE 12 (DOZE) MESES. É v±da a subconratação parcial do objeto, a da com Ouüem, a ou tran±inda, total ou parchl do contrato, bem a fusão, dsão ou da antratada, não o por nenhum compromisso assumido por aquela cnrn terceiros. SIO. A CONTRATADA ficará obrigada a aczitar, nas conYatuais acrecimos ou suprsiE que fizerem no objeto, de do valor inicial atualizado do antraü), na forrna dos SIO e 3\' do art. 65 da Lei Federal no. 8.666/93. 520. As poderão ser superiore a 25%, dsde que haja resultado de acordo entre os mnb-abnt&. CLÁUSULA SEGUNDA — PRAZO O prazo de será de 12 (doze) mses, a partir da emisSo da primeira ordem de considerando o prazo de 24 e quatro) homs para a do *Niço, após ada solicitação. CLÁUSULA TERCEIRA - PREÇO O Contra-ante pagará à Contratada 0 de R$ e seis mil reais): VLR. AL cóPIAS E IMPRESSOS COLORIDAS NO TAMANHO A3 E IMPRESS 250 G, NO TAMANHO A3 2 SRV COLORJDAS PAPEL SRV E IMPRESS , NO TAMANHO Al SRV QTD 10.om 200 UNITÁRIO R$lm R$4Ñ0 2$10,00 -2017 R$IO.OM,OO R$24.OOO,m R$2.OOO,OO PREFEITURA MUNICIPAL OE FEIRA OE SANTANA Secretaria Municipal de Administração Departamento de Licitação e Contratos PLAS E IM , NO TAMANHO A2. SRV 4 6 7 REPROGRAFICAS E IMP MONOCROMÁTICAS NOS TAMANHOS A4 , CARTA OFÍCIO REPROG FICAS E IMP COLORIDAS NOS TAMANHOS A4,CARTA OU OFÍCIO 300 600.000 60.000 ENCARD SRV , MINIMO 50 FOLHAS, PAPEL A4, R$10,oo 2$0,15 R$2,OO R$1.90 R$3.ooo,oo R$120.OOOÑO R$57.OOO,w QUANTIDADE ESTIMADA PARA O PERIODO DE 12 SRV (DOZE) MESES VALOR TOTAL: VALOR POR EXTENSO: e seis mil reais SIO - Nos previstos Contrab incluídos todos os custos material de salários, encargos g)ciais, previdenciários e trabalhistas de todo o pe•al da CONTRATADA, como também fardanmto, transporte de qualquer natureza, materiais empregados, indusive ferramentas, e utilizados, depreciação, aluguéis, administração, impostos, taxas, emolumentos e quaisquer outros cuflos que, direta ou indiretamente, se relacionem 0 fiel Cumprimento pela CONTRATADA das oygações. QUARTA - DOTAÇÃO As para o pagamento deste contrato correrão por conta dos da Orçamentária a seguir 09 -/ SECRETARIA MUNICIP EDUChCÃO - SEDUC CLÁUSULA QUINTA - PAGAMENTO DE Projeb/A8v\"ade: 12.365.047.2037 12.361.047.2036 12.122.004.2032 Elemento de 33.9{139.99 Fonte 01 Os pagamentos devidos à efetuados através de ordem barrária ou crédito em conta após apresentação da Nota Fiscal/Fatura e entrega, devidamente atestada a execução conratual, não haja pend&\'cia a ser regularizada pelo conbatado. SIO. Em havendo alguma pend&-tcia in—tiva do pagamento, será considerada data da da fira aqueia na qual ocorreu a da por parte da CONTMTADA. 520. A atualização monetária dos pagarnentos devidos pela Administração, em de mora, será calculada considerando a data do vencimento da Nota fiscal/Fatura e do seu ±tivc pagamenb, de acordo com a variação do do IBGE pro raE tempore CLÁUSULA SEXTA - MANUTENÇÃO DAS CONDIÇÓES DA PROPOSTA —REAJUSTAMENTO E REVISÃO q são fixos e irreajustáveis. CLÁUSULA SÉTIMA - OBRIGAÇÓES DA CONTRATADA A CONTRATADA, além das determinações contuas ANEXO I do Edital e decorrentes de lei, obriga- a) prestar o objeto de acordo as constantes no de licitação e no contrato, ms tocais, dias e turnos determinados Adminisfração; b) zelar pela boa e compl*a contrato e por todos os rneios ao seu alcance, a ampla ação fscalizadora dos prem*os CONTRATANTE, ptontamente às e ue lhe forem solictadas;"
+    expected_data = {
+        'name': 'M E B SERVICO E LOCACOES LTDA - ME',
+        'document': '05824468000114',
+        'legal_representant': 'DORANEDE DOS REIS SOARFS',
+        'legal_representant_document': '48001708500'
+    }
+    assert find_documents_and_names(sample) == expected_data
+
+
+@pytest.mark.parametrize('document,expected_document', [
+    ('013.876.645•22', '01387664522'),
+    ('14.043.574]0001-51', '14043574000151'),
+    ('02.921.456/0001-10', '02921456000110'),
+    ('032.668.995- 80', '03266899580'),
+])
+def test_find_documents(document, expected_document):
+    assert parse_documents(document)[0] == expected_document
