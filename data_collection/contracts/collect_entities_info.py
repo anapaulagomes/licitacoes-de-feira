fced4ee860de27bf158460160aae4506505f1f25
Ana Paula Gomes <apgomes88@gmail.com>
2019-03-10 13:33:07 +0100

    Export entities and contracts

--- data_collection/contracts/collect_entities_info.py
+++ data_collection/contracts/collect_entities_info.py
@@ -1,65 +1,165 @@
-from functools import lru_cache
-import pandas as pd
+import json
 import os
 import requests
 import requests_cache
+import rows
 import time
-from data_collection.contracts.utils import make_document_valid
+from utils import make_document_valid
 
 
-ONE_DAY_IN_SECONDS = 86400
+ONE_WEEK_IN_SECONDS = 86400 * 7
 
 
 class BrasilIOAPI:
     """
     Source: https://brasil.io/dataset/documentos-brasil/documents
     """
-    API_URL = 'https://brasil.io/api/dataset/documentos-brasil/documents/data'
-    CACHE_NAME = 'contracts/brasilio_cache'
+
+    API_URL = "https://brasil.io/api/dataset/documentos-brasil/documents/data"
+    CACHE_NAME = "contracts/brasilio_cache"
+
+    def __init__(self):
+        self.cache = requests_cache.install_cache(
+            cache_name=self.CACHE_NAME,
+            backend="sqlite",
+            expire_after=ONE_WEEK_IN_SECONDS,
+        )
 
     def get_document_info(self, document_id):
-        response = requests.get(
-            self.API_URL,
-            params={'document': document_id}
-        ).json()
-        if response['count'] == 0:
-            return {document_id: {}}
-        return  {document_id: response['results'][response['count'] - 1]}
+        response = requests.get(self.API_URL, params={"document": document_id}).json()
+        if response["count"] == 0:
+            return {}
+        return response["results"][response["count"] - 1]
 
 
 class ReceitaWs:
     """
     Source: https://receitaws.com.br/
     """
-    TOKEN = os.getenv('RECEITAWS_API_TOKEN')
-    API_URL = 'https://www.receitaws.com.br/v1/cnpj'
-    CACHE_NAME = 'contracts/brasilws_cache'
-    
+
+    TOKEN = os.getenv("RECEITAWS_API_TOKEN")
+    API_URL = "https://www.receitaws.com.br/v1/cnpj"
+    CACHE_NAME = "contracts/brasilws_cache"
+
+    def __init__(self):
+        self.cache = requests_cache.install_cache(
+            cache_name=self.CACHE_NAME,
+            backend="sqlite",
+            expire_after=ONE_WEEK_IN_SECONDS,
+        )
+
     def _request(self, document_id):
         if not document_id:
             return
         response = requests.get(
-            f'{self.API_URL}/{document_id}',
-            headers={'Authorization': f'Bearer {self.TOKEN}'}
+            f"{self.API_URL}/{document_id}",
+            headers={"Authorization": f"Bearer {self.TOKEN}"},
         ).json()
         time.sleep(20)
-        return {document_id: response}
-    
+        return response
+
     def get_document_info(self, document_id):
         try:
             return self._request(document_id)
         except:
             time.sleep(30)
 
-contracts = pd.read_excel('data/contratos-prefeitura-2016-2017.xls')
-documents = contracts['CPF/CNPJ'].unique()
-# print(f'Unique {len(documents)} documents.')
 
-search_method = BrasilIOAPI()
-requests_cache.install_cache(cache_name=search_method.CACHE_NAME, backend='sqlite', expire_after=ONE_DAY_IN_SECONDS)
+def consolidate_entities(raw_docs_from_contract):
+    docs_from_contract = {}
+    for doc in raw_docs_from_contract:
+        if docs_from_contract.get(doc.name):
+            if doc.document not in docs_from_contract[doc.name]["documents"]:
+                docs_from_contract[doc.name]["documents"].append(
+                    str(doc.document).strip(".0")
+                )
+            legal_representant = {
+                "name": doc.legal_representant,
+                "document": str(doc.legal_representant_document).strip(".0"),
+            }
+            if (
+                legal_representant
+                not in docs_from_contract[doc.name]["legal_representants"]
+            ):
+                docs_from_contract[doc.name]["legal_representants"].append(
+                    legal_representant
+                )
+        else:
+            docs_from_contract[doc.name] = {
+                "documents": [str(doc.document).strip(".0")],
+                "legal_representants": [
+                    {
+                        "name": doc.legal_representant,
+                        "document": str(doc.legal_representant_document).strip(".0"),
+                    }
+                ],
+            }
+    return docs_from_contract
 
 
-for document_id in documents:
-    document = str(document_id)
-    valid_document = make_document_valid(document)
-    print(search_method.get_document_info(valid_document))
+contracts = rows.import_from_xls("data/contracts/city-hall-contracts-2016-2017.xls")
+raw_docs_from_contract = rows.import_from_csv(
+    "data/contracts/documents-from-contracts.csv"
+)
+
+documents = set(contracts["cpfcnpj"])
+docs_from_contract = consolidate_entities(raw_docs_from_contract)
+
+brasilio = BrasilIOAPI()
+
+invalid_document = []
+entities = {}
+for contract in contracts:
+    contract_info = {
+        "code": contract.no_contrato,
+        "description": contract.objeto,
+        "start_date": contract.inicio_do_contrato,
+        "end_date": contract.fim_do_contrato,
+        "cost": contract.valorr,
+    }
+
+    # fix invalid document if invalid
+    valid_document = make_document_valid(str(contract.cpfcnpj))
+    if str(contract.cpfcnpj) != valid_document:
+        invalid_document.append(str(contract.cpfcnpj))
+
+    if entities.get(valid_document):
+        entities[valid_document]["contracts"].append(contract_info)
+    else:
+        name = contract.contratada.lstrip().rstrip()
+        entity = {
+            "name": name,  # taking into account that at least the name is right
+            "document": valid_document,
+            "document_type": "CNPJ" if len(valid_document) == 14 else "CPF",
+            "sources": ["website-contracts"],
+            "contracts": [contract_info],
+        }
+
+        # get info from parsed PDFs
+        doc_from_contract = docs_from_contract.get(contract.contratada)
+        if doc_from_contract:
+            entity["sources"].append("parsed-contracts")
+            entity["legal_representants"] = doc_from_contract["legal_representants"]
+
+        # get info on brasil io
+        result = brasilio.get_document_info(valid_document)
+        if not result and doc_from_contract:
+            for doc in doc_from_contract["documents"]:
+                if doc != valid_document:
+                    result = brasilio.get_document_info(valid_document)
+                    if result:
+                        break
+        if result:
+            entity["sources"].append("brasil-io")
+            entity["name"] = result["name"]
+            entity["document"] = result["document"]
+            entity["document_type"] = result["document_type"]
+
+        entities[valid_document] = entity
+
+
+with open("data/contracts/entities.json", "w") as outfile:
+    json.dump(entities, outfile)
+
+print(f"Unique {len(documents)} documents - Invalid: {len(set(invalid_document))}")
+print(f"Found entities: {len(entities)} Number of contracts: {len(contracts)}")
--- data_collection/contracts/parse_contract.py
+++ data_collection/contracts/parse_contract.py
@@ -4,44 +4,48 @@ import re
 import rows
 
 ONE_DAY_IN_SECONDS = 86400
-CNPJ_PATTERN = re.compile(r'\d{2}\.\d{3}\.\d{3}[\/|.]\d{4}-\d{2}')
-CPF_PATTERN = re.compile(r'\d{3}\.\d{3}\.\d{3}-\d{2}')
-CNPJ_OR_CPF_PATTERN = re.compile(r'\d{2}\S+\d{3}\S+\d{3}\S+\d{4}.*?\d{2}|\d{3}\S+\d{3}\S+\d{3}.*?\d{2}')
-CONTRACT_HEADER_PATTERN = re.compile(r'do outro lado,(.*) (condições seguintes|CLÁUSULA PRIMEIRA)')
-LEGAL_REPRESENTANT_PATTERN = re.compile(r'(representante.+?|representada por.+?) (.+?),')
+CNPJ_PATTERN = re.compile(r"\d{2}\.\d{3}\.\d{3}[\/|.]\d{4}-\d{2}")
+CPF_PATTERN = re.compile(r"\d{3}\.\d{3}\.\d{3}-\d{2}")
+CNPJ_OR_CPF_PATTERN = re.compile(
+    r"\d{2}\S+\d{3}\S+\d{3}\S+\d{4}.*?\d{2}|\d{3}\S+\d{3}\S+\d{3}.*?\d{2}"
+)
+CONTRACT_HEADER_PATTERN = re.compile(
+    r"do outro lado,(.*) (condições seguintes|CLÁUSULA PRIMEIRA)"
+)
+LEGAL_REPRESENTANT_PATTERN = re.compile(
+    r"(representante.+?|representada por.+?) (.+?),"
+)
 
 
 def clean_document(string):
-    string = re.sub(r'[^\w]', ' ', string)
-    string = string.replace(' ', '')
+    string = re.sub(r"[^\w]", " ", string)
+    string = string.replace(" ", "")
     string = string.lstrip().rstrip()
-    if len(string) == 15 and '10001' in string:
+    if len(string) == 15 and "10001" in string:
         # a few cnpjs are replacing / by 1 during PDF parser process
-        return string.replace('10001', '0001')
+        return string.replace("10001", "0001")
     return string
 
+
 def clean_string(string):
-    words = string.split(' ')
+    words = string.split(" ")
     # removing symbols in the middle of the name
     for word in words:
-        string = re.sub(r'[^\w]', ' ', string)
-    return ' '.join([
-        re.sub(r'[^\w]', ' ', word).replace(' ', '')
-        for word in words
-    ])
+        string = re.sub(r"[^\w]", " ", string)
+    return " ".join([re.sub(r"[^\w]", " ", word).replace(" ", "") for word in words])
 
 
 def parse_name(header):
-    index_name = header.find(',')
+    index_name = header.find(",")
     name = header[:index_name]
     return name.lstrip().rstrip()
 
 
 def parse_legal_representant(header):
-    salutations = ['Sr ', 'Sra ', 'Sr,(a) ', 'Sr. ', 'Sr.(a) ']
+    salutations = ["Sr ", "Sra ", "Sr,(a) ", "Sr. ", "Sr.(a) ", "Sria "]
     for salutation in salutations:
         if salutation in header:
-            header = header.replace(salutation, '')
+            header = header.replace(salutation, "")
     legal_representant = re.findall(LEGAL_REPRESENTANT_PATTERN, header)
     if legal_representant != []:
         return clean_string(legal_representant[0][1])
@@ -51,10 +55,7 @@ def parse_legal_representant(header):
 def parse_documents(header):
     documents = re.findall(CNPJ_OR_CPF_PATTERN, header)
     if documents:
-        return [
-            clean_document(document)
-            for document in documents
-        ]
+        return [clean_document(document) for document in documents]
     return []
 
 
@@ -66,7 +67,7 @@ def find_documents_and_names(sample):
 
         index_name = header.find(name)
         header_without_name = header[index_name:]
-        
+
         legal_representant = parse_legal_representant(header_without_name)
         documents = parse_documents(header)
         document = None
@@ -79,77 +80,76 @@ def find_documents_and_names(sample):
                 pass
 
         return {
-            'name': name,
-            'document': document,
-            'legal_representant': legal_representant,
-            'legal_representant_document': legal_representant_document
+            "name": name,
+            "document": document,
+            "legal_representant": legal_representant,
+            "legal_representant_document": legal_representant_document,
         }
     return {}
 
 
 def extract_content(file_content):
     pages = rows.plugins.pdf.pdf_to_text(file_content)
-    parsed_contract = ''.join(pages).replace('\r', '').replace('\n', '')
-    result = {
-        'error': None,
-        'result': {}
-    }
-    if parsed_contract != '':
-        result['result'] = find_documents_and_names(parsed_contract)
+    parsed_contract = "".join(pages).replace("\r", "").replace("\n", "")
+    result = {"error": None, "result": {}}
+    if parsed_contract != "":
+        result["result"] = find_documents_and_names(parsed_contract)
     else:
-        result['error'] = 'This PDF is probably an image'
+        result["error"] = "This PDF is probably an image"
     return result
 
 
-if __name__ == '__main__':
-    pds_directory = 'data/contracts/pdfs/'
+if __name__ == "__main__":
+    pds_directory = "data/contracts/pdfs/"
 
     not_found = 0
     image = 0
     error = 0
     key_not_found = {
-        'name': 0,
-        'document': 0,
-        'legal_representant': 0,
-        'legal_representant_document': 0,
+        "name": 0,
+        "document": 0,
+        "legal_representant": 0,
+        "legal_representant_document": 0,
     }
-    invalid_document = {
-        'document': 0,
-        'legal_representant_document': 0,
-    }
+    invalid_document = {"document": 0, "legal_representant_document": 0}
 
     pdfs = os.listdir(pds_directory)
     results = []
     for pdf in pdfs:
-        if '.pdf' in pdf:
+        if ".pdf" in pdf:
             result = extract_content(open(pds_directory + pdf))
-            result['pdf'] = pdf
 
-            if result['result'] == {}:
-                if result['error'] == 'This PDF is probably an image':
+            if result["result"] == {}:
+                if result["error"] == "This PDF is probably an image":
                     image += 1
                 else:
                     not_found += 1
                     print(pds_directory + pdf)
                 continue
-            elif result['error'] is None:
-                results.append(result['result'])
+            elif result["error"] is None:
+                output = result["result"]
+                output["pdf"] = pdf
+                results.append(output)
 
-            for key, value in result['result'].items():
-                if value == '' or value is None:
+            for key, value in result["result"].items():
+                if value == "" or value is None:
                     key_not_found[key] = key_not_found[key] + 1
-                elif key == 'document' or key == 'legal_representant_document':
+                elif key == "document" or key == "legal_representant_document":
                     if len(value) != 11 and len(value) != 14:
                         invalid_document[key] = invalid_document[key] + 1
 
-    with open('data/contracts/documents-from-contracts.csv', 'w', encoding='utf8', newline='') as output_file:
+    with open(
+        "data/contracts/documents-from-contracts.csv", "w", encoding="utf8", newline=""
+    ) as output_file:
         dict_writer = csv.DictWriter(output_file, fieldnames=results[0].keys())
         dict_writer.writeheader()
         dict_writer.writerows(results)
 
     total = len(pdfs)
-    print('------------------------------------------')
-    print(f'Total: {total} [Image: {image} ({image*100//total}%) Not found: {not_found} ({not_found*100//total}%) Error: {error} ({error*100//total}%)]')
-    print('------------------------------------------')
-    print(f'Key not found: {key_not_found}')
-    print(f'Invalid document: {invalid_document}')
+    print("------------------------------------------")
+    print(
+        f"Total: {total} [Image: {image} ({image*100//total}%) Not found: {not_found} ({not_found*100//total}%) Error: {error} ({error*100//total}%)]"
+    )
+    print("------------------------------------------")
+    print(f"Key not found: {key_not_found}")
+    print(f"Invalid document: {invalid_document}")
