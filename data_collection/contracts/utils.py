f888e31d54c74f90b2c61ecf570d996186ebaff8
Ana Paula Gomes <apgomes88@gmail.com>
2019-03-10 13:31:54 +0100

    Fix missing leading zeros

--- data_collection/contracts/utils.py
+++ data_collection/contracts/utils.py
@@ -4,15 +4,16 @@ https://www.geradorcnpj.com/algoritmo_do_cnpj.htm
 https://www.geradorcpf.com/algoritmo_do_cpf.htm
 """
 
+
 def _calculate_verifier_digit(weigths, digits):
     result = []
     for digit, weight in zip(digits, weigths):
         result.append(digit * weight)
-    
+
     magic_number = 11
 
     result_sum = sum(result)
-    quotient = result_sum//magic_number
+    quotient = result_sum // magic_number
     rest = result_sum % magic_number
 
     if rest >= 2:
@@ -30,7 +31,8 @@ def _make_document_valid(document, limit, first_weigth, second_weigth):
     digits.append(first)
 
     second = _calculate_verifier_digit(second_weigth, digits)
-    return f'{document[:limit]}{first}{second}'
+    return f"{document[:limit]}{first}{second}"
+
 
 def make_cnpj_valid(document):
     limit = 12
@@ -43,13 +45,26 @@ def make_cpf_valid(document):
     limit = 9
     first_weigth = [10, 9, 8, 7, 6, 5, 4, 3, 2]
     second_weigth = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
-    
+
     return _make_document_valid(document, limit, first_weigth, second_weigth)
 
 
 def make_document_valid(document):
+    document = fix_leading_zeros(document)
     if len(document) == 11:
         return make_cpf_valid(document)
     elif len(document) == 14:
         return make_cnpj_valid(document)
     return document
+
+
+def fix_leading_zeros(document):
+    cnpj_len = 14
+    cpf_len = 11
+    doc_len = len(document)
+    if doc_len == cnpj_len or doc_len == cpf_len:
+        return document
+    if cpf_len < doc_len < cnpj_len:
+        return "0" * (cnpj_len - doc_len) + document
+    if doc_len < cpf_len:
+        return "0" * (cpf_len - doc_len) + document
--- data_collection/tests/test_utils.py
+++ data_collection/tests/test_utils.py
@@ -1,28 +1,50 @@
-from data_collection.contracts.utils import make_cnpj_valid, make_cpf_valid, make_document_valid
+from data_collection.contracts.utils import (
+    make_cnpj_valid,
+    make_cpf_valid,
+    make_document_valid,
+    fix_leading_zeros,
+)
 import pytest
 
 
-@pytest.mark.parametrize('cnpj,expected_result', [
-    ('40521585000100', '40521585000100'),
-    ('74096231000180', '74096231000187'),
-])
+@pytest.mark.parametrize(
+    "cnpj,expected_result",
+    [("40521585000100", "40521585000100"), ("74096231000180", "74096231000187")],
+)
 def test_make_cnpj_valid(cnpj, expected_result):
     assert make_cnpj_valid(cnpj) == expected_result
 
 
-@pytest.mark.parametrize('document,expected_result', [
-    ('11144477735', '11144477735'),
-    ('25978004587', '25978004587'),
-])
+@pytest.mark.parametrize(
+    "document,expected_result",
+    [("11144477735", "11144477735"), ("25978004587", "25978004587")],
+)
 def test_make_cpf_valid(document, expected_result):
     assert make_cpf_valid(document) == expected_result
 
 
-@pytest.mark.parametrize('document,expected_result', [
-    ('11144477735', '11144477735'),
-    ('25978004587', '25978004587'),
-    ('74096231000180', '74096231000187'),
-    ('1', '1'),
-])
-def test_make_document_valid(document,expected_result):
+@pytest.mark.parametrize(
+    "document,expected_result",
+    [
+        ("11144477735", "11144477735"),
+        ("25978004587", "25978004587"),
+        ("74096231000180", "74096231000187"),
+    ],
+)
+def test_make_document_valid(document, expected_result):
     assert make_document_valid(document) == expected_result
+
+
+@pytest.mark.parametrize(
+    "document,expected_result",
+    [
+        pytest.param("5868777000196", "05868777000196", id="cnpj missing one zero"),
+        pytest.param("878183000142", "00878183000142", id="cnpj missing two zeros"),
+        pytest.param("5838479515", "05838479515", id="cpf missing one zero"),
+        pytest.param("653418566", "00653418566", id="cpf missing two zeros"),
+        pytest.param("11334909000100", "11334909000100", id="valid cnpj"),
+        pytest.param("13099647500", "13099647500", id="valid cpf"),
+    ],
+)
+def test_fix_leading_zeros(document, expected_result):
+    assert fix_leading_zeros(document) == expected_result
